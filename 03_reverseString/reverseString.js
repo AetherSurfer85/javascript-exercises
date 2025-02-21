4e9c94a398624d57b858c6e0f49e43b1a927d28f
AetherSurfer85 <135924289+AetherSurfer85@users.noreply.github.com>
2025-02-21 15:08:05 -0500

    Exercise complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const firstCharIndex = -str.length;
+  let reversedStr = '';
+  for (let i = -1; i >= firstCharIndex; i--) {
+    reversedStr = reversedStr.concat(str.at(i));
+  }
+  return reversedStr;
 };
 
 // Do not edit below this line
