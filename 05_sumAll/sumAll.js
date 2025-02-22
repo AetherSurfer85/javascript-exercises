03591c82d8d0f09569ecf9dca6b397b071420099
AetherSurfer85 <135924289+AetherSurfer85@users.noreply.github.com>
2025-02-22 10:22:59 -0500

    Exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(m, n) {
+  if (!Number.isInteger(m) || !Number.isInteger(n) || m < 0 || n < 0) { return 'ERROR' };
+  const max = Math.max(m, n);
+  const min = Math.min(m, n);
+  return (max * (max + 1) - min * (min - 1)) / 2;
 };
 
 // Do not edit below this line
