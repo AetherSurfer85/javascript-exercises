dac0a86b54846474ac9b11a875036709d6b91c52
AetherSurfer85 <135924289+AetherSurfer85@users.noreply.github.com>
2025-02-22 10:04:00 -0500

    Exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rest) {
+  let reducedArray = [];
+  const n = array.length;
+  for (let i = 0; i < n; i++) {
+    if (!rest.includes(array[i])) { reducedArray.push(array[i]) };
+  }
+  return reducedArray;
 };
 
 // Do not edit below this line
