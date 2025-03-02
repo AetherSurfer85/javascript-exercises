e2206beec6e654eebf6f3697245f064a7ab8e8ac
AetherSurfer85 <135924289+AetherSurfer85@users.noreply.github.com>
2025-03-02 15:57:25 -0500

    Exercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const thisYear = (new Date()).getFullYear();
+  let oldest = people[people.map(el => (el.yearOfDeath || thisYear) - el.yearOfBirth).reduce((iMax, el, i, arr) => el > arr[iMax] ? i : iMax, 0)];
+  return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
